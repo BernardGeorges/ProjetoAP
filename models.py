822e52f6c07b7c19cc5610c4761f1acbffd5b4c0
João Castro <castrojoao839@gmail.com>
2024-05-05 01:35:36 +0100

    explainer

--- models.py
+++ models.py
@@ -11,21 +11,24 @@ class GCNModel(torch.nn.Module):
         super(GCNModel, self).__init__()
         torch.manual_seed(12)
         self.input = None     
+        self.edge_calc = None     
         self.conv1 = GCNConv(n_features, 8)
         self.conv2 = GCNConv(8, 16)
         self.conv3 = GCNConv(16, 32)
         self.pooling = global_mean_pool
-        self.linear = Linear(32, 2)
+        self.linear = Linear(32, 2) 
+
 
     def forward(self, x, edge_index, batch):
 
         self.input = x   
+
         x = self.conv1(x, edge_index)
         x = F.relu(x)
-        x =  self.conv2(x,edge_index)
-        x =  F.relu(x)
+        x = self.conv2(x, edge_index)
+        x = F.relu(x)
         x = self.conv3(x, edge_index)
-        x = self.pooling(x,batch=batch)
+        x = self.pooling(x, batch=batch)
+                
         x = self.linear(x)
-
         return x
